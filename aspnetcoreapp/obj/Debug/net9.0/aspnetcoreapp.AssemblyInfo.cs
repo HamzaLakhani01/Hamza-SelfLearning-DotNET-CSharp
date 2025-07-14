59ce5efceaedca243ecc135068d710591f72a0f4
Hamza Lakhani <hamza.lakhani@gmail.com>
2025-07-14 13:47:03 -0500

    Final Hello World changes

--- aspnetcoreapp/obj/Debug/net9.0/aspnetcoreapp.AssemblyInfo.cs
+++ aspnetcoreapp/obj/Debug/net9.0/aspnetcoreapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aspnetcoreapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b10f352d29824471f96aa80939a13225101cd061")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+372392aad88b203e55ea00f4774b82737e2643f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("aspnetcoreapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aspnetcoreapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
